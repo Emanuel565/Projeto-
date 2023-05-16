ec2f54cc5f547caa3b9bbcf399e2a69ae32bb03c
manuca <manuca53813@gmail.com>
2023-05-16 20:58:58 -0300

    mensagem

--- system/simulador.php
+++ system/simulador.php
@@ -1,36 +1,18 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-	<link rel="stylesheet" href="../css/main.css">
-</head>
-<body>
-<div class="simulador">
+
 <?php
 
 
 		if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			$valor = $_POST["valor"];
-			$taxa = $_POST["taxa"] / 100;
+			$taxa = 1.89 / 100;
 			$prazo = $_POST["prazo"] * 12; // converte anos em meses
 			$prestacao = ($valor * $taxa) / (1 - (1 / pow(1 + $taxa, $prazo)));
 			$total = $prestacao * $prazo;
-			echo "<br><hr><br>";
-			echo "Valor do Empréstimo: R$ " . number_format($valor, 2, ",", ".");
-			echo "<br>";
-			echo "Taxa de Juros: " . number_format($taxa * 100, 2, ",", ".") . "%";
-			echo "<br>";
-			echo "Prazo de Pagamento: " . $_POST["prazo"] . " anos";
-			echo "<br><br>";
-			echo "Valor da Prestação: R$ " . number_format($prestacao, 2, ",", ".");
-			echo "<br>";
-			echo "Valor Total do Empréstimo: R$ " . number_format($total, 2, ",", ".");
+			echo "Valor do Empréstimo: R$ " . number_format($valor, 2, ",", ".").
+			"<br>".
+			"Taxa de Juros: " . number_format($taxa * 100, 2, ",", ".") . "% <br> Prazo de Pagamento: " . $_POST["prazo"] . " anos<br><br>Valor da Prestação: R$ " . number_format($prestacao, 2, ",", ".").
+			"<br>Valor Total do Empréstimo: R$ " . number_format($total, 2, ",", ".");
+			 
 		}
 	?>
-	</div>
-	
-</body>
-</html>
+
